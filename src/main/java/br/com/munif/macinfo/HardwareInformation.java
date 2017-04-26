b0d690f1c26157cc99ebac0e26cec3c25982a2d7
Munif <munifgebara@gmail.com>
2017-04-26 11:28:18 -0300

    decript

--- src/main/java/br/com/munif/macinfo/HardwareInformation.java
+++ src/main/java/br/com/munif/macinfo/HardwareInformation.java
@@ -3,7 +3,6 @@ package br.com.munif.macinfo;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.Enumeration;
 import java.util.Scanner;
 
@@ -17,34 +16,6 @@ public class HardwareInformation {
         System.out.println(encrypt);
         String decrypt = en.decrypt(encrypt);
         System.out.println("---->" + decrypt);
-    }
-
-    public static void main2(String[] args) {
-
-        try {
-            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-
-            while (networkInterfaces.hasMoreElements()) {
-                NetworkInterface network = networkInterfaces.nextElement();
-                System.out.print("----> " + network.getDisplayName() + " " + network.getName());
-                byte[] mac = network.getHardwareAddress();
-                if (mac == null) {
-                    mac = new byte[]{0, 0, 0, 0, 0, 0};
-                }
-                System.out.print("Current MAC address : ");
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < mac.length; i++) {
-                    sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
-                }
-                System.out.println(sb.toString());
-            }
-
-        } catch (SocketException e) {
-
-            e.printStackTrace();
-
-        }
-
     }
 
 }
